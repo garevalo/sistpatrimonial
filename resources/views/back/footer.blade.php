f43a443a58c277a33a877c34c0d73ac44ac94519
Giordan <giordan.arevalo@hotmail.com>
2018-04-07 18:19:50 +0000

    avance

--- resources/views/back/footer.blade.php
+++ resources/views/back/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.3.8
+        <b>Version</b> 1
     </div>
-    <strong>Copyright &copy; {{ date('Y',time()) }} <a href="/home">Gestión de Activos</a>.</strong> Municipalidad de Comas
+    <strong>Copyright &copy; {{ date('Y',time()) }} <a href="/home">Sistema Patrimonial</a>.</strong> Municipalidad de Comas
 </footer>
--- resources/views/back/header.blade.php
+++ resources/views/back/header.blade.php
@@ -1,13 +1,13 @@
 <header class="main-header">
     <!-- Logo -->
-    <a href="/" class="logo">
+    <a href="/" class="logo" style="background-color: #555299;">
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b></b>S.P</span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg">Sist. <b>{{config('app.name')}}</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
-    <nav class="navbar navbar-static-top">
+    <nav class="navbar navbar-static-top" style="background-color: #605ca8;">
         <!-- Sidebar toggle button-->
         <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Toggle navigation</span>
